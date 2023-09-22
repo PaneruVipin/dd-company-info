94ecf62be1009713b18cd998abbf4bc4f9257ca3
PaneruVipin <paneruvipinbusiness@gmail.com>
2023-09-22 02:44:13 -0700

    miner

--- src/components/scrollingPages/introduction.js
+++ src/components/scrollingPages/introduction.js
@@ -1,6 +1,3 @@
-import BackgroundImageLoader from "@/lib/backgroundImage";
-import { useEffect, useState } from "react";
-
 const Introduction = () => {
   // const [bgCount, setBgCount] = useState(2);
   // const [bg, setBg] = useState("");
@@ -17,27 +14,45 @@ const Introduction = () => {
   return (
     <>
       <div
-        className={`min-h-screen max-w-screen bg-cover bg-center font-family-1 text-[#2ac4c8] text-[3.67rem] bg-intro_image_2 p-10 `}
+        className={`min-h-[80vh] max-w-screen bg-cover bg-center font-family-1 text-[white] text-[2rem] bg-intro_image_2  `}
       >
         <p
           style={{ lineHeight: 1.1 }}
-          className={`w-[55%] break-words mt-[6%] ml-[12%]`}
+          className={`w-[55%] break-words pt-10 ml-[12%] text-center`}
         >
-          Global Temperature change (1850-2022)
+          Global Temperature change
+          <br /> (1850-2022)
         </p>
-        <div>
-          <p>Lets bring climate stripes from red to blue by decrease CO2</p>
+        <div className=" ml-[8%] mt-10 text-[3.67rem] font-thin ">
+          <p>#Bring back blue</p>
+          <p
+            style={{ lineHeight: 1.1 }}
+            className=" p-4 bg-[#23d5d7] w-fit bg-opacity-80 rounded-3xl"
+          >
+            Lets bring climate
+            <br /> stripes from red to blue <br />
+            by decrease CO2
+          </p>
         </div>
       </div>
       <div
-        className={`min-h-screen max-w-screen bg-cover bg-center font-family-1 text-[#2ac4c8] text-[3.67rem] bg-intro_image_1 p-10 `}
+        className={`flex font-family-1 items-center text-[black] px-[5%] py-[3%]`}
       >
-        <p
-          style={{ lineHeight: 1.1 }}
-          className={`w-[55%] break-words mt-[6%] ml-[12%] text-black`}
-        >
-          Global Temperature change (1850-2022)
-        </p>
+        <img
+          className="h-[80vh] rounded-md w-[33%] object-cover"
+          src="https://www.thekabadiwala.com/images/individual/plant.webp"
+        />
+        <div>
+          <h3 style={{ lineHeight: 1.1 }} className="text-[3.67rem] font-thin">
+            Know your Contribution to the environment
+          </h3>
+          <p style={{ lineHeight: 1.1 }}>
+            The Kabadiwala app allows you to check your environmental impact for
+            the amount of scrap you sold to us. The impact is shown in terms of
+            the natural resources you saved from over-exploitation or the units
+            of energy you preserved for a sustainable future.
+          </p>
+        </div>
       </div>
     </>
   );
