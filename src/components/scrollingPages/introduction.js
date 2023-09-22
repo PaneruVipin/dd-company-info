713481b1684ba9bad77fc315896790c9f9fc9b00
PaneruVipin <paneruvipinbusiness@gmail.com>
2023-09-22 04:48:52 -0700

    miner

--- src/components/scrollingPages/introduction.js
+++ src/components/scrollingPages/introduction.js
@@ -37,7 +37,7 @@ const Introduction = () => {
         className={`flex gap-x-10 justify-between font-family-1 items-center text-[black]  px-[7%] py-[3%]  bg-white`}
       >
         <img
-          className="h-[80vh] w-[40%] object-contain rounded-2xl"
+          className="h-[80vh] w-[50%] object-contain rounded-2xl"
           src="plant.jpg"
         />
         <div className="space-y-8">
@@ -55,22 +55,23 @@ const Introduction = () => {
             temperature data over a period of time, typically decades or
             centuries. They were popularized by climate scientist Ed Hawkins and
             are used to communicate the long-term trends and variations in
-            temperature. The concept behind climate stripes is simple yet
-            powerful. Each stripe in the bar represents a single year's
-            temperature, with colors indicating whether that year was warmer or
-            cooler than the long-term average. Typically, shades of blue are
-            used for cooler years, while shades of red are used for warmer
-            years. The result is a visually striking and intuitive
-            representation of temperature changes over time.
+            temperature. <br />
+            The concept behind climate stripes is simple yet powerful. Each
+            stripe in the bar represents a single year's temperature, with
+            colors indicating whether that year was warmer or cooler than the
+            long-term average. Typically, shades of blue are used for cooler
+            years, while shades of red are used for warmer years. The result is
+            a visually striking and intuitive representation of temperature
+            changes over time
           </p>
-          <div className="flex items-center gap-x-6 font-normal">
+          {/* <div className="flex items-center gap-x-6 font-normal">
             {icons.map(({ id, name, icon: Icon }) => (
               <div key={id} className="text-center space-y-4">
                 <Icon className="w-20 h-20 text-green-400 block" />
                 <span className="block">{name}</span>
               </div>
             ))}
-          </div>
+          </div> */}
         </div>
       </div>
     </>
